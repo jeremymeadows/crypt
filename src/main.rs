cc1d664a20ae96b47ad6700fb325cd82095c7413
Jeremy Meadows <jeremybmeadows@gmail.com>
2020-12-28 14:08:23 -0600

    multiple iterations of encryption

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::{io, env, fs, process};
 
 enum Mode { ENCRYPT, DECRYPT }
-// enum Error { FileNotFound, FilePermissions }
-
 use Mode::{ENCRYPT, DECRYPT};
 
 const AES_SIZE: usize = 16;
@@ -59,18 +57,27 @@ fn main() {
     let crypt: Vec<u8>;
 
     let mut key = String::new();
+    println!("Enter crypt key: ");
     io::stdin().read_line(&mut key).expect("failed to read key input");
+    println!("\u{1b}[F{}\n", String::from_utf8(vec![0x20; key.len()]).unwrap());
 
     let mut key = String::from(key).into_bytes();
     key.resize(AES_SIZE, 95);
     let key = key;
 
-    let mut msg = fs::read(&input).unwrap();
-    msg.resize(msg.len() + 16 - (msg.len() % 16), 0);
+    let mut msg = fs::read(&input).expect("failed to open file for reading");
 
     match mode {
         ENCRYPT => {
-            let msg_16 = encrypt(&key, &mut msg);
+            let ext = (16 - (msg.len() % 16)) % 16;
+            msg.resize(msg.len() + ext, 0);
+            msg.push(ext as u8);
+            msg.append(&mut vec![0u8; 15]);
+
+            let mut msg_16 = encrypt(&key, &mut msg);
+            for _ in 0..16 {
+                msg_16 = encrypt_16(&key, &mut msg_16);
+            }
 
             let mut msg: Vec<u8> = Vec::new();
             for e in msg_16 {
@@ -86,7 +93,14 @@ fn main() {
                 .into_iter()
                 .map(|e| u16::from_ne_bytes([e[0], e[1]]))
                 .collect::<Vec<u16>>();
-            let msg = decrypt(&key, &mut msg_16);
+
+            for _ in 0..16 {
+                msg_16 = decrypt_16(&key, &mut msg_16);
+            }
+            let mut msg = decrypt(&key, &mut msg_16);
+
+            let ext = msg[msg.len()-16] as usize;
+            msg.resize(msg.len() - ext - 16, 0);
 
             crypt = msg;
         }
@@ -116,6 +130,23 @@ fn encrypt(key: &Vec<u8>, msg: &mut Vec<u8>) -> Vec<u16> {
 
     msg_16
 }
+fn encrypt_16(key: &Vec<u8>, msg_16: &mut Vec<u16>) -> Vec<u16> {
+    let mut i = 0;
+    while i < msg_16.len() {
+        msg_16[i..(i+4)].rotate_left(i%4);
+        i += 4;
+    }
+
+    i = 0;
+    while i < msg_16.len() {
+        for n in 0..4 {
+            msg_16[i+n] += key[(i+n) % key.len()] as u16;
+        }
+        i += 4;
+    }
+
+    msg_16.to_vec()
+}
 
 fn decrypt(key: &Vec<u8>, msg_16: &mut Vec<u16>) -> Vec<u8> {
     let mut i = 0;
@@ -143,4 +174,26 @@ fn decrypt(key: &Vec<u8>, msg_16: &mut Vec<u16>) -> Vec<u8> {
     }
 
     msg
+}
+fn decrypt_16(key: &Vec<u8>, msg_16: &mut Vec<u16>) -> Vec<u16> {
+    let mut i = 0;
+    while i < msg_16.len() {
+        for n in 0..4 {
+            let k = key[(i+n) % key.len()] as u16;
+            if msg_16[i+n] >= k {
+                msg_16[i+n] -= k;
+            }
+        }
+        i += 4;
+    }
+    msg_16.reverse();
+
+    i = 0;
+    while i < msg_16.len() {
+        msg_16[i..(i+4)].rotate_right(i%4);
+        i += 4;
+    }
+    msg_16.reverse();
+
+    msg_16.to_vec()
 }
